cef963be0a88a59d3ee5b1514d922f4a8e9fd36d
Paras Gupta <parasg1999@gmail.com>
2022-12-07 08:12:42 +0530

    fix: logo, banner, date

---
+++ data/educationData.ts
@@ -0,0 +1,15 @@
+const educationData = [
+  {
+    title: 'Computer Science Engineering',
+    company: 'Indian Institute of Information Technology, Una',
+    location: 'Mumbai, India',
+    range: 'July 2017 - June 2021',
+    url: 'https://iiitu.ac.in/',
+    description: [
+      'Graduated with CGPA of 9.28 with <a href="https://iiitu.ac.in/wp-content/uploads/2022/09/IIITU_Medal-Winners-2017-21.pdf">Department Rank 3</a>',
+      'Spent 4 years <s>learning (Physics!? 👀)</s> and tinkering with libraries, frameworks, and databases.',
+    ],
+  },
+]
+
+export default educationData
---
+++ data/experienceData.ts
@@ -0,0 +1,21 @@
+const experienceData = [
+  {
+    title: 'Backend Engineer',
+    company: 'Spenny',
+    location: 'Bengaluru, India',
+    range: 'March 2022 - Present',
+    url: 'https://spenny.com/',
+    description: [
+    ],
+  }, {
+    title: 'Software Development Engineer',
+    company: 'Amazon',
+    location: 'Remote',
+    range: 'September 2021 - March 2022',
+    url: 'https://amazon.com/',
+    description: [
+    ],
+  },
+]
+
+export default experienceData
